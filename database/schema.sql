b6259cc6c325b9dab43d2ed759bc6250c72f4176
Wendyddw <arielddw@gmail.com>
2022-03-22 19:38:31 -0700

    modified id to integer for ETL

--- database/schema.sql
+++ database/schema.sql
@@ -4,23 +4,21 @@ CREATE DATABASE sdc;
 
 DROP TABLE IF EXISTS product CASCADE;
 
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
 CREATE TABLE product (
-  id uuid DEFAULT uuid_generate_v4 (),
+  id INTEGER NOT NULL,
   name VARCHAR(50) NOT NULL,
   slogan VARCHAR(100) NOT NULL,
   description VARCHAR(1000) NOT NULL,
-  category VARCHAR(100) NOT NULL,
-  default_price VARCHAR(100) NOT NULL,
+  category VARCHAR(1000) NOT NULL,
+  default_price INTEGER NOT NULL,
   PRIMARY KEY (id)
 );
 
 DROP TABLE IF EXISTS features CASCADE;
 
 CREATE TABLE features (
-  id uuid DEFAULT uuid_generate_v4 (),
-  product_id uuid NOT NULL,
+  id INTEGER NOT NULL,
+  product_id INTEGER NOT NULL,
   feature VARCHAR(100) NOT NULL,
   value VARCHAR(100) NOT NULL,
   PRIMARY KEY (id),
@@ -32,12 +30,12 @@ CREATE TABLE features (
 DROP TABLE IF EXISTS styles CASCADE;
 
 CREATE TABLE styles (
-  id uuid DEFAULT uuid_generate_v4 (),
-  product_id uuid NOT NULL,
+  id INTEGER NOT NULL,
+  product_id INTEGER NOT NULL,
   name VARCHAR(100) NOT NULL,
   original_price VARCHAR(100) NOT NULL,
   sale_price VARCHAR(100) NOT NULL,
-  "default?" BOOLEAN NOT NULL,
+  default_style BOOLEAN NOT NULL,
   PRIMARY KEY (id),
   CONSTRAINT product_id FOREIGN KEY (product_id)
       REFERENCES product(id)
@@ -47,8 +45,8 @@ CREATE TABLE styles (
 DROP TABLE IF EXISTS photos CASCADE;
 
 CREATE TABLE photos (
-  id uuid DEFAULT uuid_generate_v4 (),
-  style_id uuid NOT NULL,
+  id INTEGER NOT NULL,
+  style_id INTEGER NOT NULL,
   thumbnail_url VARCHAR(1000),
   url VARCHAR(1000),
   PRIMARY KEY (id),
@@ -60,8 +58,8 @@ CREATE TABLE photos (
 DROP TABLE IF EXISTS skus CASCADE;
 
 CREATE TABLE skus (
-  id uuid DEFAULT uuid_generate_v4 (),
-  style_id uuid NOT NULL,
+  id INTEGER NOT NULL,
+  style_id INTEGER NOT NULL,
   quantity INTEGER NOT NULL,
   size VARCHAR(20) NOT NULL,
   PRIMARY KEY (id),
@@ -73,9 +71,9 @@ CREATE TABLE skus (
 DROP TABLE IF EXISTS related CASCADE;
 
 CREATE TABLE related (
-  id uuid DEFAULT uuid_generate_v4 (),
-  current_product_id uuid NOT NULL,
-  related_product_id uuid NOT NULL,
+  id INTEGER NOT NULL,
+  current_product_id INTEGER NOT NULL,
+  related_product_id INTEGER NOT NULL,
   PRIMARY KEY (id),
   CONSTRAINT current_product_id FOREIGN KEY (current_product_id)
     REFERENCES product(id)
