f30b20c26f8b5452b5fb1d7f11b30a73613b3510
Wendyddw <arielddw@gmail.com>
2022-03-22 23:53:38 -0700

    completed ETL

--- database/schema.sql
+++ database/schema.sql
@@ -33,8 +33,8 @@ CREATE TABLE styles (
   id INTEGER NOT NULL,
   product_id INTEGER NOT NULL,
   name VARCHAR(100) NOT NULL,
-  original_price VARCHAR(100) NOT NULL,
-  sale_price VARCHAR(100) NOT NULL,
+  sale_price INTEGER,
+  original_price INTEGER NOT NULL,
   default_style BOOLEAN NOT NULL,
   PRIMARY KEY (id),
   CONSTRAINT product_id FOREIGN KEY (product_id)
@@ -47,8 +47,8 @@ DROP TABLE IF EXISTS photos CASCADE;
 CREATE TABLE photos (
   id INTEGER NOT NULL,
   style_id INTEGER NOT NULL,
-  thumbnail_url VARCHAR(1000),
   url VARCHAR(1000),
+  thumbnail_url CHARACTER VARYING(10485760) DEFAULT NULL,
   PRIMARY KEY (id),
   CONSTRAINT style_id FOREIGN KEY (style_id)
       REFERENCES styles(id) MATCH SIMPLE
@@ -60,8 +60,8 @@ DROP TABLE IF EXISTS skus CASCADE;
 CREATE TABLE skus (
   id INTEGER NOT NULL,
   style_id INTEGER NOT NULL,
-  quantity INTEGER NOT NULL,
   size VARCHAR(20) NOT NULL,
+  quantity INTEGER NOT NULL,
   PRIMARY KEY (id),
   CONSTRAINT style_id FOREIGN KEY (style_id)
       REFERENCES styles(id) MATCH SIMPLE
@@ -82,5 +82,31 @@ CREATE TABLE related (
 
 COPY product (id, name, slogan, description, category, default_price)
 FROM '/Users/wendai/Desktop/hackreactorwork/SDC_CSV/product.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY features (id, product_id, feature, value)
+FROM '/Users/wendai/Desktop/hackreactorwork/SDC_CSV/features.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY styles (id, product_id, name, sale_price, original_price, default_style)
+FROM '/Users/wendai/Desktop/hackreactorwork/SDC_CSV/styles.csv'
+WITH (FORMAT CSV,DELIMITER ',',
+NULL 'null',
+HEADER);
+
+COPY photos (id, style_id, url, thumbnail_url)
+FROM '/Users/wendai/Desktop/hackreactorwork/SDC_CSV/photos.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY skus (id, style_id, size, quantity)
+FROM '/Users/wendai/Desktop/hackreactorwork/SDC_CSV/skus.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY related (id, current_product_id, related_product_id)
+FROM '/Users/wendai/Desktop/hackreactorwork/SDC_CSV/related.csv'
 DELIMITER ','
 CSV HEADER;
