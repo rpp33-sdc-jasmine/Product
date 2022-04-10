ef6e1eedc34765da76a0d17441e3f4e76c773981
Wendyddw <arielddw@gmail.com>
2022-04-10 00:14:59 -0700

    minor change

--- database/database.js
+++ database/database.js
@@ -21,23 +21,7 @@ const allProducts = async (page, count, callback) => {
     }
   })
 }
-/*
-const productInfo = async (product_id, callback) => {
-  const queryString = `SELECT product.id, product.name, product.slogan, product.description, product.category, product.default_price::text, json_agg(json_build_object('feature', features.feature, 'value', features.value)) AS features
-  FROM product
-  LEFT JOIN features
-  ON product.id = features.product_id
-  WHERE product.id = ${product_id}
-  GROUP BY product.id, product.name, product.slogan, product.description, product.category, product.default_price`;
-  await pool.query(queryString, (err,res) => {
-    if(err) {
-      console.log(err.message);
-    } else {
-      callback(null, res.rows);
-    }
-  })
-}
-*/
+
 const productInfo = async (product_id, callback) => {
   const queryString = `SELECT product.id, product.name, product.slogan, product.description, product.category, product.default_price::text,
   (SELECT json_agg(json_build_object('feature',feature, 'value', value )) AS features
