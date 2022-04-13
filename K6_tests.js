ebf0469e5f15242af6cb7cecb1d917190519f77a
Wen Dai <wendai@Wens-MacBook-Pro-2.local>
2022-04-12 22:38:48 -0700

    k6 test ongoing

--- K6_tests.js
+++ K6_tests.js
@@ -1,13 +1,22 @@
 import http from "k6/http";
 import { check, sleep } from "k6";
 export let options = {
-  vus: 10,
-  duration: "10s"
+  vus: 300,
+  duration: "30s"
 };
+/*export default function() {
+  //or use product id 999999
+  //const rnd = Math.floor(Math.floor(Math.random() * 1000000));
+  let res = http.get('http://localhost:3000/products/999999');
+  check(res, {
+    "success": (r) => r.status == 200
+  });
+};*/
+
 export default function() {
   //or use product id 999999
-  const rnd = Math.floor(Math.floor(Math.random() * 1000000));
-  let res = http.get(`http://localhost:3000/products/${rnd}`);
+  //const rnd = Math.floor(Math.floor(Math.random() * 1000000));
+  let res = http.get('http://localhost:3000/products/999998/styles');
   check(res, {
     "success": (r) => r.status == 200
   });
